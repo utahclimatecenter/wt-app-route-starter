adb3961cfe5a3fca3566885e08a4bc1bd5656a6d
Martin Israelsen <martin@Martins-iMac.local>
2017-11-27 21:38:08 -0700

    Adding initial starter app

---
+++ gitcheckout.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+git clone https://github.com/utahclimatecenter/wt-bootstrap-styles.git ./bower_components/wt-bootstrap-styles
+cd bower_components/wt-bootstrap-styles/
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+
+git clone https://github.com/utahclimatecenter/wt-highcharts.git ./bower_components/wt-highcharts
+cd bower_components/wt-highcharts/ 
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+ 
+git clone https://github.com/utahclimatecenter/wt-master-slave-combo.git ./bower_components/wt-master-slave-combo
+cd bower_components/wt-master-slave-combo/ 
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+
+git clone https://github.com/utahclimatecenter/wt-sortable-list.git ./bower_components/wt-sortable-list
+cd bower_components/wt-sortable-list
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+
+git clone https://github.com/utahclimatecenter/wt-srch-bar.git ./bower_components/wt-srch-bar
+cd bower_components/wt-srch-bar
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+
+git clone https://github.com/utahclimatecenter/wt-vaadin-multi-combo.git ./bower_components/wt-vaadin-multi-combo
+cd bower_components/wt-vaadin-multi-combo
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+
+git clone https://github.com/utahclimatecenter/wt-srch-grid.git ./bower_components/wt-srch-grid
+cd bower_components/wt-srch-grid
+git fetch origin
+git checkout release/1.0 
+cd ../../
+pwd
+
